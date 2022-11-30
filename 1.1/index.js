1e8601ef31f87ff5d454ece37891836590e0fbc1
Arthur Rosch <rosch.arthur@gmail.com>
2022-11-30 17:04:11 -0300

    Exec Finalizado

---
+++ 1.1/index.js
@@ -0,0 +1,27 @@
+const validator = require("validator");
+const readXlsxFile = require("read-excel-file/node");
+
+var arrayName = [],
+  arrayEmails = [],
+  arrayYears = [],
+  arrayEnrollment = [];
+
+readXlsxFile("./Alunos.xlsx").then((rows) => {
+  for (let index = 0; index < rows.length; index++) {
+    let check = false;
+    const name = rows[index][0];
+    const email = rows[index][1];
+    const years = rows[index][2];
+    const enrollment = rows[index][3];
+
+    validator.isAlpha(name) && (check = true);
+    validator.isEmail(email) && (check = true);
+    years > 0 && (check = true);
+    validator.isAlphanumeric(enrollment) && (check = true);
+
+    check === true &&
+      console.log(
+        `Nome ${name}\nEmail: ${email}\nIdade: ${years}\nMatricula: ${enrollment}`
+      );
+  }
+});
