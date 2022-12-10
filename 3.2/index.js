698d83276941fca0753cb167b272374ad543a432
Arthur Rosch <rosch.arthur@gmail.com>
2022-12-10 16:01:29 -0300

    Exec Finalizado

--- 3.2/index.js
+++ 3.2/index.js
@@ -29,8 +29,60 @@ submit.addEventListener("click", (e) => {
     color.value,
     auto.value
   );
+  listCar.push(newCar);
   brand.value = "";
   model.value = "";
   year.value = "";
   color.value = "";
 });
+function searchByBrand(brandName, arrayOfCars) {
+  let searchedBrandsList = [];
+  arrayOfCars.forEach((element) => {
+    element.Brand === brandName
+      ? searchedBrandsList.push(element)
+      : console.log("Essa Marca não tem em nosso estoque");
+  });
+}
+function brandsAvailable(arrayOfCars) {
+  let brandsAvailable = arrayOfCars.filter((a, i) => {
+    arr.indexOf(a) === i;
+  });
+  return brandsAvailable;
+}
+function ShowProperties(array, arrayStrings) {
+  let ListOfCarsProperties = [];
+
+  array.forEach((item) => {
+    let newObject = {};
+    arrayStrings.forEach((string) => {
+      switch (string) {
+        case "Brand":
+          const Brand = item[string];
+          newObject.Brand = Brand;
+          break;
+
+        case "Model":
+          const Model = item[string];
+          newObject.Model = Model;
+          break;
+
+        case "Year":
+          const Year = item[string];
+          newObject.Year = Year;
+          break;
+
+        case "Value":
+          const Value = item[string];
+          newObject.Value = Value;
+          break;
+
+        case "Color":
+          const Color = item[string];
+          newObject.Color = Color;
+          break;
+      }
+    });
+    ListOfCarsProperties.push(newObject);
+  });
+  return ListOfCarsProperties;
+}
